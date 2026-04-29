1018b084c504a46562e202b39b6cdc6bfcff0879
nickinicole1365-cpu <nickinicole1365@gmail.com>
2026-04-29 16:52:02 -0600

    git

--- Site/Site/settings.py
+++ Site/Site/settings.py
@@ -67,9 +67,9 @@ WSGI_APPLICATION = 'Site.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
-        ssl_require=True
+        ssl_require=False  # importante en local
     )
 }
 
